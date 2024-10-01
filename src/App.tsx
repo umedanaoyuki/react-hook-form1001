fc79c3b29fd1ec25558c8aad59f7ec4c193dbeda
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-10-01 23:22:16 +0900

    ラジオボタンの選択によって表示を切り替える

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,61 @@
+import { MyFormType, useMyForm } from "./use-my-form";
+
 function App() {
-  return <>テスト</>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+  } = useMyForm();
+
+  const participantType = watch("participantType");
+
+  const onSubmit = (data: MyFormType) => {
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label htmlFor="">お名前は？</label>
+      <input type="" {...register("name")} />
+      {errors.name && <span>{errors.name.message}</span>}
+
+      <label htmlFor="">参加者タイプ</label>
+      <input
+        type="radio"
+        {...register("participantType")}
+        id="participantType_timeTraveler"
+        value="timeTraveler"
+      />
+      <label htmlFor="participantType_timeTraveler">
+        時間旅行者 - Time Traveler
+      </label>
+      <input
+        type="radio"
+        {...register("participantType")}
+        id="participantType_alien"
+        value="alien"
+      />
+      <label htmlFor="participantType_alien">エイリアン — Alien</label>
+
+      {participantType === "timeTraveler" && (
+        <>
+          <label>西暦何年から来ましたか？</label>
+          <input {...register("year")} />
+          {errors.year && <span>{errors.year.message}</span>}
+        </>
+      )}
+
+      {participantType === "alien" && (
+        <>
+          <label htmlFor="">出身星の名前は?</label>
+          <input {...register("planetName")} />
+          {errors.planetName && <span>{errors.planetName.message}</span>}
+        </>
+      )}
+      <button type="submit">登録</button>
+    </form>
+  );
 }
 
 export default App;
---
+++ src/use-my-form.ts
@@ -0,0 +1,28 @@
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+const myFormSchema = yup.object().shape({
+  name: yup.string().required("お名前を入力してください。"),
+  participantType: yup.string().oneOf(["timeTraveler", "alien"]).required(),
+  year: yup.number().when("participantType", {
+    is: (val: string) => val == "timeTraveler",
+    then: (schema) =>
+      schema.required().min(2025, "2025年以降を入力してください"),
+    otherwise: (schema) => schema.notRequired(),
+  }),
+  planetName: yup.string().when("participantType", {
+    is: (val: string) => val == "alien",
+    then: (schema) =>
+      schema.required().matches(/星|座/, "星か星座の名前を入力してください。"),
+    otherwise: (schema) => schema.notRequired(),
+  }),
+});
+
+export type MyFormType = yup.InferType<typeof myFormSchema>;
+
+export const useMyForm = () => {
+  return useForm<MyFormType>({
+    resolver: yupResolver(myFormSchema),
+  });
+};
